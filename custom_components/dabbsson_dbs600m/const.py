0e0051b72ece8a66038f3416e00a41bc4f52660d
kleimj1 <jochen.kleimann@gmail.com>
2025-05-26 23:02:54 +0200

    Create const.py

---
+++ custom_components/dabbsson_dbs600m/const.py
@@ -0,0 +1,9 @@
+"""Konstanten für die Dabbsson DBS600M Integration."""
+
+DOMAIN = "dabbsson_dbs600m"
+
+DEFAULT_NAME = "Dabbsson DBS600M"
+MANUFACTURER = "Dabbsson"
+MODEL = "DBS600M"
+
+ICON = "mdi:solar-power"
