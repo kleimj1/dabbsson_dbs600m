eb241fe89119b32122cb526648a089b66a4fe659
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 00:40:54 +0200

    Create number.py

---
+++ custom_components/dabbsson_dbs600m/number.py
@@ -0,0 +1,33 @@
+from homeassistant.components.number import NumberEntity
+from .api import get_device_properties, set_device_property
+from .const import DOMAIN
+from .dps_metadata import DPS_METADATA
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    device_id = config_entry.data["device_id"]
+    headers = config_entry.data["headers"]
+    data = get_device_properties(device_id, headers)
+    numbers = [DabbssonNumber(dp, device_id, headers) for dp in data if dp["code"] in DPS_METADATA and DPS_METADATA[dp["code"]]["type"] == "value" and DPS_METADATA[dp["code"]].get("writable")]
+    async_add_entities(numbers, True)
+
+class DabbssonNumber(NumberEntity):
+    def __init__(self, dp, device_id, headers):
+        self._device_id = device_id
+        self._headers = headers
+        self._code = dp.get("code")
+        self._value = dp.get("value")
+        meta = DPS_METADATA[self._code]
+        self._attr_name = meta.get("name", self._code)
+        self._attr_unique_id = f"dbs600m_{dp.get('dp_id')}"
+        self._attr_unit_of_measurement = meta.get("unit")
+        self._attr_native_min_value = meta.get("min", 0)
+        self._attr_native_max_value = meta.get("max", 100)
+        self._attr_native_step = meta.get("step", 1)
+
+    @property
+    def native_value(self):
+        return self._value
+
+    def set_native_value(self, value):
+        set_device_property(self._device_id, self._headers, self._code, value)
+        self._value = value
