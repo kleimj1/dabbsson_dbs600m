e56df0924afbb7ab29103d119dba073ebdbc26ea
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 00:31:32 +0200

    Update __init__.py

--- custom_components/dabbsson_dbs600m/__init__.py
+++ custom_components/dabbsson_dbs600m/__init__.py
@@ -1,37 +1,13 @@
-## Datei: __init__.py
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from .const import DOMAIN
 
-import logging
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    return True
 
-from homeassistant.const import CONF_NAME
-from homeassistant.core import callback
-from homeassistant.helpers.discovery import async_load_platform
-
-from .const import DOMAIN, CONF_DEVICES, CONF_MQTT_TOPIC
-
-_LOGGER = logging.getLogger(__name__ + ".dabbsson_dbs600m")
-
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_DEVICES): [
-                    {
-                        vol.Required(CONF_NAME): cv.string,
-                        vol.Required(CONF_MQTT_TOPIC): cv.string,
-                    }
-                ]
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
-
-async def async_setup(hass, config):
-    devices = config[DOMAIN][CONF_DEVICES]
-    for device in devices:
-        hass.async_create_task(
-            async_load_platform(hass, "sensor", DOMAIN, device, config)
-        )
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
