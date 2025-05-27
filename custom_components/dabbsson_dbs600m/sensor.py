8c4de06a322db55a800648047b0349d384bb2726
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 00:42:16 +0200

    Update sensor.py

--- custom_components/dabbsson_dbs600m/sensor.py
+++ custom_components/dabbsson_dbs600m/sensor.py
@@ -1,21 +1,24 @@
 from homeassistant.components.sensor import SensorEntity
 from .api import get_device_properties
 from .const import DOMAIN
+from .dps_metadata import DPS_METADATA
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     device_id = config_entry.data["device_id"]
     headers = config_entry.data["headers"]
     data = get_device_properties(device_id, headers)
-    sensors = [DabbssonSensor(dp) for dp in data if dp["type"] == "value"]
+    sensors = [DabbssonSensor(dp) for dp in data if dp["code"] in DPS_METADATA and DPS_METADATA[dp["code"]]["type"] == "value" and not DPS_METADATA[dp["code"]].get("writable")]
     async_add_entities(sensors, True)
 
 class DabbssonSensor(SensorEntity):
     def __init__(self, dp):
-        self._attr_name = dp.get("code")
+        meta = DPS_METADATA.get(dp["code"], {})
+        self._attr_name = meta.get("name", dp.get("code"))
         self._attr_unique_id = f"dbs600m_{dp.get('dp_id')}"
         self._dp_id = dp.get("dp_id")
         self._code = dp.get("code")
-        self._unit = dp.get("unit", "")
+        self._unit = meta.get("unit", "")
+        self._description = meta.get("description", "")
         self._value = dp.get("value")
 
     @property
