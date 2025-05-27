4be6a493d110dc374686197764cc5964fb65d23d
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 00:30:38 +0200

    Update config_flow.py

--- custom_components/dabbsson_dbs600m/config_flow.py
+++ custom_components/dabbsson_dbs600m/config_flow.py
@@ -1,25 +1,21 @@
-import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
-from .const import DOMAIN, CONF_MQTT_TOPIC
+from .const import DOMAIN, CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_ACCESS_TOKEN, CONF_DEVICE_ID
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
 class DabbssonConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     async def async_step_user(self, user_input=None):
-        errors = {}
         if user_input is not None:
-            return self.async_create_entry(
-                title=user_input[CONF_NAME],
-                data=user_input,
-            )
-
-        data_schema = vol.Schema({
-            vol.Required(CONF_NAME): str,
-            vol.Required(CONF_MQTT_TOPIC): str,
-        })
+            return self.async_create_entry(title="DBS600M", data=user_input)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=data_schema,
-            errors=errors
+            data_schema=vol.Schema({
+                vol.Required(CONF_CLIENT_ID): str,
+                vol.Required(CONF_CLIENT_SECRET): str,
+                vol.Required(CONF_ACCESS_TOKEN): str,
+                vol.Required(CONF_DEVICE_ID): str,
+            })
         )
