6d1a717584aa98c52b055d05fed0b0aef8357096
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 09:01:18 +0200

    Update config_flow.py

--- custom_components/dabbsson_dbs600m/config_flow.py
+++ custom_components/dabbsson_dbs600m/config_flow.py
@@ -1,13 +1,14 @@
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
-from .const import DOMAIN, CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_ACCESS_TOKEN, CONF_DEVICE_ID
+from .const import DOMAIN, CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_DEVICE_ID
 import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
 
 class DabbssonConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
+
     async def async_step_user(self, user_input=None):
         if user_input is not None:
+            # Hier wird direkt das Entry erstellt – Token wird später in der API geholt
             return self.async_create_entry(title="DBS600M", data=user_input)
 
         return self.async_show_form(
