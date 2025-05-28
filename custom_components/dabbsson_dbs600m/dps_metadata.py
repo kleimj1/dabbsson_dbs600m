3091dc9e351a4d57c3ddee9a984ae3afc391f24a
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 09:22:44 +0200

    Update dps_metadata.py

--- custom_components/dabbsson_dbs600m/dps_metadata.py
+++ custom_components/dabbsson_dbs600m/dps_metadata.py
@@ -2,171 +2,171 @@ DPS_METADATA = {
     "1": {
         "name": "Gesamtverbrauch (kWh)",
         "type": "value",
-        "writable": true,
+        "writable": True,
         "description": "Gesamtverbrauch (kWh)",
         "unit": "kW·h"
     },
     "3": {
         "name": "PV1 Parameter",
         "type": "raw",
-        "writable": false,
+        "writable": False,
         "description": "PV1 Parameter"
     },
     "13": {
         "name": "Wechselrichter-Modell",
         "type": "string",
-        "writable": false,
+        "writable": False,
         "description": "Wechselrichter-Modell"
     },
     "14": {
         "name": "Wechselrichter Seriennummer",
         "type": "string",
-        "writable": false,
+        "writable": False,
         "description": "Wechselrichter Seriennummer"
     },
     "15": {
         "name": "IMEI/IMSI",
         "type": "string",
-        "writable": false,
+        "writable": False,
         "description": "IMEI/IMSI"
     },
     "16": {
         "name": "Aktuelle Temperatur",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "Aktuelle Temperatur",
         "unit": "℃"
     },
     "28": {
         "name": "Leistungsdaten",
         "type": "raw",
-        "writable": true,
+        "writable": True,
         "description": "Leistungsdaten"
     },
     "29": {
         "name": "Betriebsstatus",
         "type": "raw",
-        "writable": true,
+        "writable": True,
         "description": "Betriebsstatus"
     },
     "101": {
         "name": "PV-Eingangsleistung",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "PV-Eingangsleistung",
         "unit": "W"
     },
     "102": {
         "name": "CO2-Einsparung",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "CO2-Einsparung",
         "unit": "Kg"
     },
     "103": {
         "name": "PV-Spannung",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "PV-Spannung",
         "unit": "V"
     },
     "104": {
         "name": "Inverter-Temperatur",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "Inverter-Temperatur",
         "unit": "℃"
     },
     "105": {
         "name": "Zähler zurücksetzen",
         "type": "bool",
-        "writable": true,
+        "writable": True,
         "description": "Zähler zurücksetzen"
     },
     "106": {
         "name": "AC-Strom",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "AC-Strom",
         "unit": "A"
     },
     "107": {
         "name": "Tagesverbrauch",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "Tagesverbrauch",
         "unit": "kW·h"
     },
     "108": {
         "name": "Wechselrichter EIN/AUS",
         "type": "bool",
-        "writable": true,
+        "writable": True,
         "description": "Wechselrichter EIN/AUS"
     },
     "109": {
         "name": "AC-Ausgangsleistung",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "AC-Ausgangsleistung",
         "unit": "W"
     },
     "110": {
         "name": "Leistungsbegrenzung",
         "type": "value",
-        "writable": true,
+        "writable": True,
         "description": "Leistungsbegrenzung",
         "unit": "%"
     },
     "111": {
         "name": "PV-Strom",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "PV-Strom",
         "unit": "A"
     },
     "112": {
         "name": "Virtuell gepflanzte Bäume",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "Virtuell gepflanzte Bäume",
         "unit": "pcs"
     },
     "113": {
         "name": "Gesamtenergieerzeugung",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "Gesamtenergieerzeugung",
         "unit": "kW·h"
     },
     "114": {
         "name": "AC-Spannung",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "AC-Spannung",
         "unit": "V"
     },
     "115": {
         "name": "Steckdosen Online-Status",
         "type": "string",
-        "writable": false,
+        "writable": False,
         "description": "Steckdosen Online-Status"
     },
     "116": {
         "name": "Steckdosenleistung",
         "type": "string",
-        "writable": false,
+        "writable": False,
         "description": "Steckdosenleistung"
     },
     "117": {
         "name": "Steckdosenverbrauch",
         "type": "raw",
-        "writable": false,
+        "writable": False,
         "description": "Steckdosenverbrauch"
     },
     "118": {
         "name": "Energiequelle",
         "type": "enum",
-        "writable": false,
+        "writable": False,
         "description": "Energiequelle",
         "options": [
             "0",
@@ -177,33 +177,33 @@ DPS_METADATA = {
     "119": {
         "name": "Anti-Feed-In-Schalter",
         "type": "bool",
-        "writable": true,
+        "writable": True,
         "description": "Anti-Feed-In-Schalter"
     },
     "120": {
         "name": "Batterie-Kapazität",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "Batterie-Kapazität",
         "unit": "%"
     },
     "121": {
         "name": "Gesamtleistung der Steckdosen",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "Gesamtleistung der Steckdosen",
         "unit": "W"
     },
     "122": {
         "name": "Knoten hinzufügen",
         "type": "bool",
-        "writable": true,
+        "writable": True,
         "description": "Knoten hinzufügen"
     },
     "123": {
         "name": "PV lädt Batterie",
         "type": "enum",
-        "writable": false,
+        "writable": False,
         "description": "PV lädt Batterie",
         "options": [
             "0",
@@ -213,19 +213,19 @@ DPS_METADATA = {
     "124": {
         "name": "Gepaarte Knoten",
         "type": "string",
-        "writable": false,
+        "writable": False,
         "description": "Gepaarte Knoten"
     },
     "125": {
         "name": "Alle Knoten löschen",
         "type": "bool",
-        "writable": true,
+        "writable": True,
         "description": "Alle Knoten löschen"
     },
     "126": {
         "name": "Arbeitsmodus",
         "type": "enum",
-        "writable": true,
+        "writable": True,
         "description": "Arbeitsmodus",
         "options": [
             "0",
@@ -235,20 +235,20 @@ DPS_METADATA = {
     "127": {
         "name": "Nennleistung (W)",
         "type": "value",
-        "writable": false,
+        "writable": False,
         "description": "Nennleistung (W)",
         "unit": "W"
     },
     "128": {
         "name": "Schnellberichtsmodus",
         "type": "bool",
-        "writable": true,
+        "writable": True,
         "description": "Schnellberichtsmodus"
     },
     "146": {
         "name": "INV-Modus setzen",
         "type": "value",
-        "writable": true,
+        "writable": True,
         "description": "INV-Modus setzen",
         "unit": ""
     }
