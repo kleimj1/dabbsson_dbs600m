a83f0eab7549b68e1b8b42ecce70bc4540a31e58
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 09:02:20 +0200

    Update const.py

--- custom_components/dabbsson_dbs600m/const.py
+++ custom_components/dabbsson_dbs600m/const.py
@@ -1,6 +1,7 @@
 DOMAIN = "dbs600m"
+
 CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"
-CONF_ACCESS_TOKEN = "access_token"
 CONF_DEVICE_ID = "device_id"
+
 API_BASE_URL = "https://openapi.tuyaeu.com"
