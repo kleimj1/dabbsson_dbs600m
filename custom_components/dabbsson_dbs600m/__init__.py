06029c45accfa5a00eacec70bbd0143ab75af2ca
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 09:17:31 +0200

    Update __init__.py

--- custom_components/dabbsson_dbs600m/__init__.py
+++ custom_components/dabbsson_dbs600m/__init__.py
@@ -9,10 +9,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
