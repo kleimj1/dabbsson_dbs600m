6737d5195a67d74315174058da637c2da70eb859
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 08:58:27 +0200

    Update number.py

--- custom_components/dabbsson_dbs600m/number.py
+++ custom_components/dabbsson_dbs600m/number.py
@@ -1,19 +1,29 @@
 from homeassistant.components.number import NumberEntity
-from .api import get_device_properties, set_device_property
+from .api import TuyaCloudAPI
 from .const import DOMAIN
 from .dps_metadata import DPS_METADATA
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
+    client_id = config_entry.data["client_id"]
+    client_secret = config_entry.data["client_secret"]
     device_id = config_entry.data["device_id"]
-    headers = config_entry.data["headers"]
-    data = get_device_properties(device_id, headers)
-    numbers = [DabbssonNumber(dp, device_id, headers) for dp in data if dp["code"] in DPS_METADATA and DPS_METADATA[dp["code"]]["type"] == "value" and DPS_METADATA[dp["code"]].get("writable")]
+
+    api = TuyaCloudAPI(client_id, client_secret)
+    data = api.get_device_properties(device_id)
+
+    numbers = [
+        DabbssonNumber(dp, api, device_id)
+        for dp in data
+        if dp["code"] in DPS_METADATA
+        and DPS_METADATA[dp["code"]]["type"] == "value"
+        and DPS_METADATA[dp["code"]].get("writable")
+    ]
     async_add_entities(numbers, True)
 
 class DabbssonNumber(NumberEntity):
-    def __init__(self, dp, device_id, headers):
+    def __init__(self, dp, api, device_id):
         self._device_id = device_id
-        self._headers = headers
+        self._api = api
         self._code = dp.get("code")
         self._value = dp.get("value")
         meta = DPS_METADATA[self._code]
@@ -29,5 +39,6 @@ class DabbssonNumber(NumberEntity):
         return self._value
 
     def set_native_value(self, value):
-        set_device_property(self._device_id, self._headers, self._code, value)
-        self._value = value
+        success = self._api.set_device_property(self._device_id, self._code, value)
+        if success:
+            self._value = value
