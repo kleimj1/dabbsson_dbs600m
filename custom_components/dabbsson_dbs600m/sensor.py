f1ccf1ae8549f93422ea5de8f08a7722ef2ceeef
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 08:57:01 +0200

    Update sensor.py

--- custom_components/dabbsson_dbs600m/sensor.py
+++ custom_components/dabbsson_dbs600m/sensor.py
@@ -1,17 +1,27 @@
 from homeassistant.components.sensor import SensorEntity
-from .api import get_device_properties
+from .api import TuyaCloudAPI
 from .const import DOMAIN
 from .dps_metadata import DPS_METADATA
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
+    client_id = config_entry.data["client_id"]
+    client_secret = config_entry.data["client_secret"]
     device_id = config_entry.data["device_id"]
-    headers = config_entry.data["headers"]
-    data = get_device_properties(device_id, headers)
-    sensors = [DabbssonSensor(dp) for dp in data if dp["code"] in DPS_METADATA and DPS_METADATA[dp["code"]]["type"] == "value" and not DPS_METADATA[dp["code"]].get("writable")]
+
+    api = TuyaCloudAPI(client_id, client_secret)
+    data = api.get_device_properties(device_id)
+
+    sensors = [
+        DabbssonSensor(dp, api, device_id)
+        for dp in data
+        if dp["code"] in DPS_METADATA
+        and DPS_METADATA[dp["code"]]["type"] == "value"
+        and not DPS_METADATA[dp["code"]].get("writable")
+    ]
     async_add_entities(sensors, True)
 
 class DabbssonSensor(SensorEntity):
-    def __init__(self, dp):
+    def __init__(self, dp, api, device_id):
         meta = DPS_METADATA.get(dp["code"], {})
         self._attr_name = meta.get("name", dp.get("code"))
         self._attr_unique_id = f"dbs600m_{dp.get('dp_id')}"
@@ -20,13 +30,15 @@ class DabbssonSensor(SensorEntity):
         self._unit = meta.get("unit", "")
         self._description = meta.get("description", "")
         self._value = dp.get("value")
+        self._api = api
+        self._device_id = device_id
 
     @property
     def native_value(self):
         return self._value
 
     def update(self):
-        properties = get_device_properties(self._dp_id)
+        properties = self._api.get_device_properties(self._device_id)
         for dp in properties:
             if dp.get("dp_id") == self._dp_id:
                 self._value = dp.get("value")
