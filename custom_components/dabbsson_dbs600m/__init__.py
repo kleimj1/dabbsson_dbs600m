60150df5320a8cd0ca533a512943183d188c852c
kleimj1 <jochen.kleimann@gmail.com>
2025-05-28 09:05:11 +0200

    Update __init__.py

--- custom_components/dabbsson_dbs600m/__init__.py
+++ custom_components/dabbsson_dbs600m/__init__.py
@@ -2,12 +2,12 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from .const import DOMAIN
+from .api import TuyaCloudAPI
+
+PLATFORMS = ["sensor", "switch", "number", "select"]
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
-    return True
+    client_id = entry.data["client_id"]
