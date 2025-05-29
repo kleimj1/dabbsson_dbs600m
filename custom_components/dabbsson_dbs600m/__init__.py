7dcaa9b90db74199cc474f58aa42b10cec9011fa
kleimj1 <jochen.kleimann@gmail.com>
2025-05-30 00:28:07 +0200

    Update __init__.py

--- custom_components/dabbsson_dbs600m/__init__.py
+++ custom_components/dabbsson_dbs600m/__init__.py
@@ -1,17 +1,36 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-from .const import DOMAIN
+from .const import DOMAIN, PLATFORMS, DATA_COORDINATOR, DATA_DEVICE
+from .api import TuyaDeviceApi, DabbssonCoordinator
 
-PLATFORMS = ["sensor", "switch", "number", "select"]
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Setup der Dabbsson DBS600M Integration über das UI."""
+    hass.data.setdefault(DOMAIN, {})
+
+    api = TuyaDeviceApi(
+        entry.data["client_id"],
+        entry.data["client_secret"],
+        entry.data["device_id"],
+        entry.data["api_endpoint"],
+    )
+    api.connect()
+
+    coordinator = DabbssonCoordinator(hass, api, entry.data["name"])
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        DATA_COORDINATOR: coordinator,
+        DATA_DEVICE: api,
+    }
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Entferne eine Integration."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
     return unload_ok
