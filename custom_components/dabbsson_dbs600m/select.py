61fa805fa2e9d05ab7e4a91cb825d17196c77c0a
kleimj1 <jochen.kleimann@gmail.com>
2025-05-30 10:24:57 +0200

    Aktualisieren von select.py

--- custom_components/dabbsson_dbs600m/select.py
+++ custom_components/dabbsson_dbs600m/select.py
@@ -11,10 +11,8 @@ _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    """Setze alle enum-Optionen als SelectEntity."""
     coordinator = hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR]
     api = hass.data[DOMAIN][entry.entry_id][DATA_DEVICE]
-
     entities = []
 
     for dps_code, meta in DPS_METADATA.items():
@@ -25,24 +23,20 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
 
 class DabbssonSelect(CoordinatorEntity, SelectEntity):
-    """Dropdown-Auswahl für enum-DPS."""
-
     def __init__(self, coordinator, api, dps_code: str, meta: dict):
         super().__init__(coordinator)
         self.api = api
         self._dps_code = dps_code
         self._meta = meta
-
         self._attr_name = meta["name"]
         self._attr_unique_id = f"{api.device_id}_{dps_code}"
         self._attr_options = meta.get("options", [])
 
     @property
     def current_option(self) -> str:
-        """Gibt die aktuell gewählte Option zurück."""
         return str(self.coordinator.data.get(self._dps_code))
 
     async def async_select_option(self, option: str):
-        """Setzt eine neue Option."""
-        if await self.api.send_command(self._dps_code, option):
+        code = self._meta.get("code", self._dps_code)
+        if await self.api.send_command(code, option):
             await self.coordinator.async_request_refresh()
