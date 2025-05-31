206ac214ae22d1881f6bc286bc88bc7ac8e949f6
kleimj1 <jochen.kleimann@gmail.com>
2025-05-31 22:52:38 +0200

    Update const.py

--- custom_components/dabbsson_dbs600m/const.py
+++ custom_components/dabbsson_dbs600m/const.py
@@ -1,22 +1,20 @@
+"""Konstanten für die Dabbsson DBS600M Integration."""
+
 DOMAIN = "dabbsson_dbs600m"
 
-# Unterstützte Plattformen
-PLATFORMS = ["sensor", "switch", "number", "select"]
-
-# Konfigurationsoptionen
+# Konfigurationsschlüssel
 CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"
 CONF_DEVICE_ID = "device_id"
-CONF_NAME = "name"
 CONF_API_ENDPOINT = "api_endpoint"
+CONF_NAME = "name"
 
-# Tuya Europe API-Standardendpunkte
+# Standardwerte
 DEFAULT_API_ENDPOINT = "https://openapi.tuyaeu.com"
-DEFAULT_MQ_ENDPOINT = "wss://mqe.tuyaeu.com:8285/"
 
-# Weitere Konstanten
+# Gemeinsame Datenablagen
 DATA_COORDINATOR = "coordinator"
 DATA_DEVICE = "device"
 
-# Versionsinfo
-INTEGRATION_VERSION = "1.0.0"
+# Version
+INTEGRATION_VERSION = "2.1.1"
